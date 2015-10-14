7e769161d3e99a0a74bcad2591a5b6c5c49e0edc
Rodrigo Costa <rodrigocosta168@hotmail.com>
2015-10-14 14:33:11 -0300

    Initial Commit

---
+++ TestJenkins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestJenkins
+//
+//  Created by Rodrigo Costa on 14/10/15.
+//  Copyright (c) 2015 Universidade Positivo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
