05b2de8b40bd130ee6bacf95dfba29be6c00b383
Rodrigo Costa <rodrigocosta168@hotmail.com>
2015-10-14 18:05:53 -0300

    test

--- TestJenkinsTests/TestJenkinsTests.swift
+++ TestJenkinsTests/TestJenkinsTests.swift
@@ -23,7 +23,7 @@ class TestJenkinsTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        XCTAssert(false, "Pass")
     }
     
     func testPerformanceExample() {
